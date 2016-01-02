0b02e976d3221ab9b1d26d29ba28c8d94dc39895
Zhiming Zhou <zzm.vmiis@gmail.com>
2016-01-02 18:25:07 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 <script>
-  alert('Hello World!');
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", 'https://github.com/vmiis/Unigreen-Food-Pty-Ltd/edit/master/start.html', false);
+  xmlhttp.send();
+  var html=xmlhttp.responseText;
+  alert(html);
 </script>
