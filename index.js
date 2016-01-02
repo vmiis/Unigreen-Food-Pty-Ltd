4ced82ed09d4d775b3a2143e3e349a68e0ad2c6b
vmiis <zzm.vmiis@gmail.com>
2016-01-03 01:11:24 +1100

    Update

--- index.js
+++
@@ -1,8 +0,0 @@
-<script>
-  var url='https://raw.githubusercontent.com/vmiis/Unigreen-Food-Pty-Ltd/master/start.html';
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", url, false);
-  xmlhttp.send();
-  var html=xmlhttp.responseText;
-  document.write(html);
-</script>
