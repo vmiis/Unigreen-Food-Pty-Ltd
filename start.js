91dcb83e4f5f936f62f99f5661951620f0aa2ca6
Zhiming Zhou <zzm.vmiis@gmail.com>
2016-01-08 16:27:30 +1100

    update

---
+++ start.js
@@ -0,0 +1,36 @@
+<script>
+(function(){
+      $vm.start=function(site_base){
+      	//----------------------------------------
+      	//Init: where, debug, version...
+            alert(site_base);
+      	var app_base           =site_base+"Unigreen-Food-Pty-Ltd/";
+      	var common_base        =site_base+"Common-Modules/";
+      	var production_ver     ='20160106';
+            g_server               ="development";
+      	var debug              =true;
+            var api_base           ="https://cbs.wappsystem.com/dev/";
+            var code_base          ="https://github.com/vmiis/Unigreen-Food-Pty-Ltd/blob/master/";
+      	//----------------------------------------
+      	$vm.server=g_server;
+      	$vm.app_base=app_base;
+      	$vm.code_base=code_base;
+      	$vm.common_base=common_base;
+      	$vm.ver=production_ver; if(debug) $vm.ver=new Date().getTime();
+      	$VmAPI.api_base=api_base;
+      	//-------------------------------------------------------------------------
+      	//first do some init tasks, then load the main layout
+      	$vm.init({callback:function(){
+      		var param={
+      			pid:Math.random().toString().replace('.',''),
+      			slot:$vm.root_layout_content_slot,
+      			url:$vm.app_base+'modules/main_layout.html',
+      			source:'modules/main_layout.html'
+      		}
+      		$vm.load_module(param);
+      		if(g_server=="development") $('#_help_').show();
+      	}});
+      	//-------------------------------------------------------------------------
+      }
+}());
+</script>
