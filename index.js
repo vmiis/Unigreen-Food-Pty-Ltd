02bb91d2cd079c70037b77def264e216c3c4eda1
Zhiming Zhou <zzm.vmiis@gmail.com>
2016-01-22 14:21:45 +1100

    update

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 $vm.module_list={
-    main_layout:                 ['--------','__BASE__/wappsystem/Unigreen-Food-Pty-Ltd/modules/main_layout.html','https://github.com/wappsystem/Unigreen-Food-Pty-Ltd/blob/master/modules/main_layout.html'],
-    home:                        ['--------','__BASE__/wappsystem/Unigreen-Food-Pty-Ltd/modules/home.html','https://github.com/wappsystem/Unigreen-Food-Pty-Ltd/blob/master/modules/home.html'],
-    business_portal_nav:         ['--------','__BASE__/wappsystem/Unigreen-Food-Pty-Ltd/modules/business_portal_nav.html','https://github.com/wappsystem/Unigreen-Food-Pty-Ltd/blob/master/modules/business_portal_nav.html'],
-    production_list:             ['20007681','__BASE__/wappsystem/Unigreen-Food-Pty-Ltd/modules/production_list.html','https://github.com/wappsystem/Unigreen-Food-Pty-Ltd/blob/master/modules/unigreen/modules/production_list.html'],
-    staff_list:                  ['20007682','__BASE__/wappsystem/Unigreen-Food-Pty-Ltd/modules/staff_list.html','https://github.com/wappsystem/Unigreen-Food-Pty-Ltd/blob/master/modules//unigreen/modules/staff_list.html'],
+    main_layout:        ['--------','.../main_layout.html'],
+    home:               ['--------','.../home.html'],
+    business_portal_nav:['--------','.../business_portal_nav.html'],
+    production_list:    ['20007681','.../production_list.html'],
+    staff_list:         ['20007682','.../staff_list.html'],
 }
 //--------------------------------------------------------
+for(key in $vm.module_list){
+    $vm.module_list[key].push($vm.module_list[key][1]);
+    $vm.module_list[key][1]=$vm.module_list[key][1].replace('...','__BASE__/'+$vm.repository+'/modules')
+    $vm.module_list[key][2]=$vm.module_list[key][2].replace('...','https://github.com/'+$vm.repository+'/master/module')
+}
 var last=function(){
     $('head').append("<style> *{ margin:0; } html,body { height:100%;} </style>");
     setTimeout(function () {
@@ -21,6 +26,5 @@ var last=function(){
         $('head').append("<link rel='stylesheet' media='screen' href='https://cbs.wappsystem.com/system/third/handsontable.full.min.css'>");
         $('head').append("<link rel='stylesheet' href='https://ajax.aspnetcdn.com/ajax/jquery.ui/1.11.4/themes/redmond/jquery-ui.css'>");
     }, 100);
-
 }
 //--------------------------------------------------------
